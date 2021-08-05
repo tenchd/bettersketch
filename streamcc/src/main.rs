473d233d788b16815efaa2456f1a1a85b51d4dc3
tenchd <dtench@protonmail.ch>
2021-08-05 16:50:50 -0400

    expanded benchmark

--- streamcc/src/main.rs
+++ streamcc/src/main.rs
@@ -33,9 +33,19 @@ fn main() {
 
 fn speed_updates(vec_length: u64) {
     let mut x = XorSketch::new(vec_length,0.5_f64,1);
-    for i in 0..1000000 {
-        x.update(i);
+    let mut range = 1000000;
+    let mut reps = 1;    
+    if vec_length < 1000000 {
+        range = vec_length;
+        reps = 1000000/range;
     }
+    for _j in 0..reps{
+        for i in 0..range {
+            x.update(i);
+        }
+    }
+    x.update(1);
+    
     println!("{}", x.query().expect("Sampling failed."))
 }
 
@@ -130,7 +140,82 @@ mod tests {
     use test::Bencher;
 
     #[bench]
-    fn bench_sketch_updates(b: &mut Bencher) {
+    fn bench_sketch_updates3(b: &mut Bencher) {
+        b.iter(|| speed_updates(1000));
+    }
+    
+    #[bench]
+    fn bench_sketch_updates4(b: &mut Bencher) {
+        b.iter(|| speed_updates(10000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates5(b: &mut Bencher) {
+        b.iter(|| speed_updates(100000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates6(b: &mut Bencher) {
         b.iter(|| speed_updates(1000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates7(b: &mut Bencher) {
+        b.iter(|| speed_updates(10000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates8(b: &mut Bencher) {
+        b.iter(|| speed_updates(100000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates9(b: &mut Bencher) {
+        b.iter(|| speed_updates(1000000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates10(b: &mut Bencher) {
+        b.iter(|| speed_updates(10000000000));
+    }
+    
+    #[bench]
+    fn bench_sketch_updates11(b: &mut Bencher) {
+        b.iter(|| speed_updates(100000000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates12(b: &mut Bencher) {
+        b.iter(|| speed_updates(1000000000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates13(b: &mut Bencher) {
+        b.iter(|| speed_updates(10000000000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates14(b: &mut Bencher) {
+        b.iter(|| speed_updates(100000000000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates15(b: &mut Bencher) {
+        b.iter(|| speed_updates(1000000000000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates16(b: &mut Bencher) {
+        b.iter(|| speed_updates(10000000000000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates17(b: &mut Bencher) {
+        b.iter(|| speed_updates(10000000000000000));
+    }
+
+    #[bench]
+    fn bench_sketch_updates18(b: &mut Bencher) {
+        b.iter(|| speed_updates(1000000000000000000));
     }
 }
